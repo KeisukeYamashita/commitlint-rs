12ff1291a9585e61998d2d4b83ce7526985768de
KeisukeYamashita <19yamashita15@gmail.com>
2023-06-14 19:48:45 +0900

    feat(cli): initial linter
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+mod message;
+mod result;
+
+use args::Args;
+use clap::Parser;
+
+
+use std::process::exit;
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let messages = match args.read() {
+        Ok(messages) => messages,
+        Err(err) => {
+            eprintln!("Failed to read commit messages: {}", err);
+            exit(1)
+        }
+    };
+
+    let threads = messages
+        .into_iter()
+        .map(|message| tokio::spawn(async move { message.lint().await }))
+        .collect::<Vec<_>>();
+
+    let results = futures::future::join_all(threads).await;
+    println!("{:?}", results)
 }
---
+++ src/message.rs
@@ -0,0 +1,23 @@
+use std::fmt::Error;
+
+use crate::result::Result as Res;
+
+/// Message represents a single commit message.
+#[derive(Debug)]
+pub struct Message {
+    /// Raw commit message (or any input from stdin).
+    pub raw: String,
+}
+
+impl Message {
+    /// Create a new Message.
+    pub fn new(raw: String) -> Self {
+        Self { raw }
+    }
+
+    /// Lint the raw commit message.
+    pub async fn lint(&self) -> Result<Res, Error> {
+        // TODO: Implement linting.
+        Ok(Res { valid: true })
+    }
+}
