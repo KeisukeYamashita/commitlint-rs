7303f23e97fedf10fd150e949b14b8d6847d9716
KeisukeYamashita <19yamashita15@gmail.com>
2024-12-03 00:33:12 +0100

    fix(cli): remove generate_json_schema from cli package
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -125,17 +125,3 @@ async fn load_unknown_config_file(path: PathBuf) -> Result<Config, String> {
         path.display()
     ))
 }
-#[cfg(test)]
-mod tests {
-    #[cfg(feature = "schemars")]
-    #[test]
-    // TODO assert insta snapshot
-    fn generate_json_schema() {
-        use crate::config::Config;
-        use std::fs;
-
-        let config_schema = schemars::schema_for!(Config);
-        let config_schema_json = serde_json::to_string_pretty(&config_schema).unwrap();
-        fs::write("json-schema/config.json", config_schema_json).unwrap();
-    }
-}
